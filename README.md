f6ae2eae3484520d5e42a6782c0ed4e2b72634ad
bhoff <bruce.hoff@sagebase.org>
2018-09-30 20:15:26 -0700

    works

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # oryhydrastub
+
 This is a test implementation of the services required to make Ory Hydra work.  See https://www.ory.sh/docs/guides/master/hydra
+
+
+To run the end-to-end test:
+
+(1) start up Ory Hydra and the stubbed out auth service:
+
+```
+docker-compose up
+```
+(2) Register the client ID and secret:
+
+```
+docker run --rm -it \
+  -e HYDRA_ADMIN_URL=http://10.0.0.87:9001 \
+  oryd/hydra:v1.0.0-beta.9 \
+  clients create --skip-tls-verify \
+    --id demo-client-id \
+    --secret demo-client-secret \
+    --grant-types authorization_code,refresh_token,client_credentials,implicit \
+    --response-types token,code,id_token \
+    --scope openid,offline,demo.scope \
+    --callbacks http://127.0.0.1:9010/callback
+```
+(3) Start the client app:
+
+```
+docker run --rm -it \
+  -p 9010:9010 \
+  oryd/hydra:v1.0.0-beta.9 \
+  token user --skip-tls-verify \
+    --port 9010 \
+    --auth-url http://10.0.0.87:9000/oauth2/auth \
+    --token-url http://10.0.0.87:9000/oauth2/token \
+    --client-id demo-client-id  \
+    --client-secret demo-client-secret \
+    --scope openid,offline,demo.scope
+```
+
+Note:  This demo currently does not use TLS, does not use a Docker network to connect the containers, and uses only an in-memory database.  For more info on this demo, see: 
+
+[The authentication and consent flow](https://www.ory.sh/docs/guides/latest/hydra/3-overview/1-oauth2#authenticating-users-and-requesting-consent)
+
+[The system set-up instructions](https://www.ory.sh/docs/guides/master/hydra/4-install/)
+
+[The javascript implementation of what we have stubbed out here in Java](https://github.com/ory/hydra-login-consent-node)
+
+[The source code for the server](https://github.com/ory/hydra)
+
--- src/main/java/authprovider/AuthProvider.java
+++ src/main/java/authprovider/AuthProvider.java
@@ -1,8 +1,16 @@
 
 package authprovider;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -10,10 +18,25 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AuthProvider extends HttpServlet {
 	private Logger logger = Logger.getLogger("DockerAuth");
@@ -31,8 +54,10 @@ public class AuthProvider extends HttpServlet {
 		}
 	}
 		
+	private static final String HYDRA_ENDPOINT = getProperty("HYDRA_ENDPOINT");
+
 	private void doGetIntern(HttpServletRequest req, HttpServletResponse resp)
-				throws IOException {
+				throws IOException, KeyManagementException, UnrecoverableKeyException, UnsupportedOperationException, JSONException, NoSuchAlgorithmException, KeyStoreException {
 		int i = req.getRequestURI().indexOf(req.getServletPath());
 		if (i<0)
 			throw new IllegalArgumentException("ServletPath: "+req.getServletPath()+", Request URI: "+req.getRequestURI());
@@ -43,14 +68,137 @@ public class AuthProvider extends HttpServlet {
 			paramMap.put(key, Arrays.asList(((Object[])req.getParameterMap().get(key))));
 		}
 		if ("/login".equalsIgnoreCase(action)) {
-			logger.log(Level.INFO, paramMap.toString());
-			resp.setStatus(307);			
+			logger.info(paramMap.toString());
+			String loginChallenge = req.getParameter("login_challenge");
+			if (StringUtils.isEmpty(loginChallenge)) throw new IllegalArgumentException("missing login_challenge request parameter.");
+			// GET "https://localhost:9001/oauth2/auth/requests/login/"+loginChallenge
+			// see the javascript example here:
+			// https://github.com/ory/hydra-login-consent-node/blob/322b7f631bb8c58c9998cd5f130ced0ea9496465/routes/login.js
+			// this provides add'l info, esp. 'skip', which tells us not to prompt the user to log in
+			HttpGet get = new HttpGet(HYDRA_ENDPOINT+"/oauth2/auth/requests/login/"+loginChallenge);
+			JSONObject getResult = executeRequest(get, 200);
+			logger.info("Login request returned "+getResult);
+			boolean skip = false;
+			if (getResult.has("skip")) skip = getResult.getBoolean("skip");
+			
+			String requestBody = null;
+			if (skip) {
+				requestBody = "{\"subject\":\"user-name\"}";
+			} else {
+				requestBody = "{\"subject\":\"user-name\",\"remember\":true,\"remember_for\":3600}";
+			}
+			HttpPut put = new HttpPut(HYDRA_ENDPOINT+"/oauth2/auth/requests/login/"+loginChallenge+"/accept"); //... or +"/reject"
+			HttpEntity httpEntity = new StringEntity(requestBody, ContentType.APPLICATION_JSON);
+			put.setEntity(httpEntity);
+			JSONObject result = executeRequest(put, 200);
+			if (StringUtils.isEmpty( result.getString("redirect_to"))) throw new IllegalStateException("No redirect_to in "+result);
+			resp.setHeader("Location", result.getString("redirect_to"));
+			resp.setStatus(302);
+			logger.info("Redirecting to "+result.getString("redirect_to"));
 		} else if ("/consent".equalsIgnoreCase(action)) {
-			logger.log(Level.INFO, paramMap.toString());
-			resp.setStatus(307);			
-		} else {
-			resp.setStatus(404);			
-		}
+			logger.info(paramMap.toString());
+			String consentChallenge = req.getParameter("consent_challenge");
+			if (StringUtils.isEmpty(consentChallenge)) throw new IllegalArgumentException("missing consent_challenge request parameter.");
 
+			
+			HttpGet get = new HttpGet(HYDRA_ENDPOINT+"/oauth2/auth/requests/consent/"+consentChallenge);
+			JSONObject getResult = executeRequest(get, 200);
+			logger.info("Consent request returned "+getResult);
+			JSONObject putBody = new JSONObject();
+			putBody.put("grant_scope", getResult.get("requested_scope"));
+			HttpPut put = new HttpPut(HYDRA_ENDPOINT+"/oauth2/auth/requests/consent/"+consentChallenge+"/accept");  //... or +"/reject"
+			logger.info("Putting to URL: "+put.getURI()+", body: "+putBody.toString());
+			HttpEntity httpEntity = new StringEntity(putBody.toString(), ContentType.APPLICATION_JSON);
+			put.setEntity(httpEntity);
+			JSONObject putResult = executeRequest(put, 200);
+			if (StringUtils.isEmpty(putResult.getString("redirect_to"))) throw new IllegalStateException("No redirect_to in "+putResult);
+			resp.setHeader("Location", putResult.getString("redirect_to"));
+			resp.setStatus(302);			
+			logger.info("Redirecting to "+putResult.getString("redirect_to"));
+		} else {
+			resp.setStatus(404);
+		}
+	}
+	
+	
+	private static void checkHttpResponseCode(HttpResponse response, int expected) throws IOException {
+		if (expected!=response.getStatusLine().getStatusCode()) {
+			InputStream inputStream = response.getEntity().getContent();
+			StringBuffer result = new StringBuffer();
+			try {
+				BufferedReader rd = new BufferedReader(
+						new InputStreamReader(inputStream));
+
+				String line = "";
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+			} finally {
+				inputStream.close();
+			}
+			throw new RuntimeException("Expected "+expected+" but received "+
+					response.getStatusLine().getStatusCode()+" Response body: "+result.toString());	
+		}
+	}
+	
+	
+	private static JSONObject getResponseBodyAsJson(HttpResponse response) throws UnsupportedOperationException, IOException, JSONException {
+		InputStream inputStream = response.getEntity().getContent();
+		StringBuffer result = new StringBuffer();
+		try {
+			BufferedReader rd = new BufferedReader(
+					new InputStreamReader(inputStream));
+
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+		} finally {
+			inputStream.close();
+		}
+		return new JSONObject(result.toString());
+	}
+	
+	private static HttpClient getHttpClient() throws KeyManagementException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
+		org.apache.http.conn.ssl.TrustStrategy acceptingTrustStrategy = new org.apache.http.conn.ssl.TrustStrategy() {
+			public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				return true;
+			}};
+	    		
+	    SSLSocketFactory sf = new SSLSocketFactory(acceptingTrustStrategy);
+	    
+	    HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}};
+	    
+		return HttpClientBuilder.create().setSSLSocketFactory(sf).setSSLHostnameVerifier(hostnameVerifier).build();
+	}
+	public JSONObject executeRequest(final HttpRequestBase request, final int expecteResponseCode) throws UnsupportedOperationException, IOException, JSONException, KeyManagementException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {		
+		HttpResponse response = getHttpClient().execute(request);
+		checkHttpResponseCode(response, expecteResponseCode);
+		return getResponseBodyAsJson(response);
+	}
+
+	private static boolean missing(String s) {
+		return StringUtils.isEmpty(s) || "null".equals(s);
+	}
+
+
+	public static String getProperty(String key) {
+		return getProperty(key, true);
+	}
+	public static String getProperty(String key, boolean required) {
+		{
+			String commandlineOption = System.getProperty(key);
+			if (!missing(commandlineOption)) return commandlineOption;
+		}
+		{
+			String environmentVariable = System.getenv(key);
+			if (!missing(environmentVariable)) return environmentVariable;
+		}
+		if (required) throw new RuntimeException("Cannot find value for "+key);
+		return null;
 	}
 }
